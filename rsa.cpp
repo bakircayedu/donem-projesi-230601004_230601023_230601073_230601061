cbfe39478c9dfd784f4e24c0031d13de69d26152
Akif Akbulut <akifakb@gmail.com>
2024-03-03 02:11:00 +0300

    Added primeGen() to generate prime numbers / Asal sayi uretmek icin primeGen() eklendi

--- rsa.cpp
+++ rsa.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <cmath>
+#include <ctime>
+#include <cstdlib>
 
 using namespace std;
 
 bool isPrime(unsigned long int);
+int primeGen();
 
 int main(){
 
@@ -11,12 +14,28 @@ int main(){
     
 }
 
+int primeGen(){
+  
+  srand((unsigned) time(0));
+
+  unsigned long int randomNumber;
+
+  do{
+    randomNumber = rand();
+  }while(isPrime(randomNumber) == 0);
+
+  return randomNumber;
+
+}
+
 bool isPrime(unsigned long int num){
     
     unsigned long int max = sqrt(num);
+
     if(num%2==0){
         return false;
     }
+
     for(int i = 3; i <= max; i+=2){
         if(num%i==0){
             return false;
