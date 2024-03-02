e2eb13c12be8bd622eb373d41189c47f173b3999
Akif Akbulut <akifakb@gmail.com>
2024-03-03 01:41:36 +0300

    Added isPrime() to check primality of the given number / Girilen sayinin asalligini kontrol etmek icin isPrime() eklendi

--- rsa.cpp
+++ rsa.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
+bool isPrime(unsigned long int);
+
 int main(){
+
+
     
 }
+
+bool isPrime(unsigned long int num){
+    
+    unsigned long int max = sqrt(num);
+    if(num%2==0){
+        return false;
+    }
+    for(int i = 3; i <= max; i+=2){
+        if(num%i==0){
+            return false;
+        }
+    }
+    return true;
+
+}
+
