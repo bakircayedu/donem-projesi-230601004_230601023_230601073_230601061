5bb4bcaed85535e01b79bdc2b1035906255b5004
Akif Akbulut <akifakb@gmail.com>
2024-03-03 12:48:08 +0300

    Changed the random number generation method to generate bigger integers / Daha buyuk tam sayilar elde edebilmek icin random sayi generatoru degistirildi

--- rsa.cpp
+++ rsa.cpp
@@ -1,42 +1,56 @@
 #include <iostream>
 #include <cmath>
-#include <ctime>
-#include <cstdlib>
+#include <random>
 
 using namespace std;
 
-bool isPrime(unsigned long int);
-int primeGen();
+bool isPrime(unsigned long long int);
+unsigned long long int* primeGen();
+
+
 
 int main(){
 
+    unsigned long long int* a = primeGen();
+    cout<<*a<<endl;
+    cout<<*(a+1);
 
     
 }
 
-int primeGen(){
-  
-  srand((unsigned) time(0));
+unsigned long long int* primeGen(){
 
-  unsigned long int randomNumber;
+    unsigned long long int randNum;
 
-  do{
-    randomNumber = rand();
-  }while(isPrime(randomNumber) == 0);
+    static unsigned long long int primes[2];
+    
+    random_device rd;
+    default_random_engine generator(rd());
+    unsigned long long int max = -1;
+    unsigned long int min = -1;
 
-  return randomNumber;
+    uniform_int_distribution<long long unsigned> distribution(min,max);
+    for(int i = 0; i<=1; i++){
+        do{
+            randNum= distribution(generator);
+        }while(isPrime(randNum)==0);
+        primes[i] = randNum;
+    }
+
+
+    return primes;
 
 }
 
-bool isPrime(unsigned long int num){
+bool isPrime(unsigned long long int num){
     
-    unsigned long int max = sqrt(num);
+    unsigned long long int max = sqrt(num);
 
     if(num%2==0){
         return false;
     }
 
-    for(int i = 3; i <= max; i+=2){
+    for(unsigned long long int i = 3; i <= max; i+=2){
         if(num%i==0){
             return false;
         }
